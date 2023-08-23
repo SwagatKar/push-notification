091a6e0777375780a7a512d77f71d211345929a7
SwagatKar <s.kar.swagat@gmail.com>
2023-08-23 21:41:48 +0530

    init

---
+++ src/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyDQP0eh5GPfM85f1MbTk6TtbCk5X8W7p_4",
+      authDomain: "swagattest-de85d.firebaseapp.com",
+      projectId: "swagattest-de85d",
+      storageBucket: "swagattest-de85d.appspot.com",
+      messagingSenderId: "277354202736",
+      appId: "1:277354202736:web:c91e072cb520bb7380d6b5",
+      measurementId: "G-MQ21HYSEPW",
+      vapidKey: "BESanABnO1GG-5_d3PU_jU5VtJ3j1-pT70XchKOCibVUQvui_iji4zravoY26QT2I5cHyq9QkCp2kMwjmVDXUXc"
+    },
+};
---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/10.3.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging-compat.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyDQP0eh5GPfM85f1MbTk6TtbCk5X8W7p_4",
+    authDomain: "swagattest-de85d.firebaseapp.com",
+    projectId: "swagattest-de85d",
+    storageBucket: "swagattest-de85d.appspot.com",
+    messagingSenderId: "277354202736",
+    appId: "1:277354202736:web:c91e072cb520bb7380d6b5",
+    measurementId: "G-MQ21HYSEPW"
+});
+const messaging = firebase.messaging();
